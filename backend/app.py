1c9967ec97d5382a4a32bdc6de50b56a007d56b2
emman123 <67055497+mantot-123@users.noreply.github.com>
2026-04-15 22:01:51 +0100

    initial commit

---
+++ backend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/upload")
+def uploadAudioFile():
+    # TODO HANDLE FILE UPLOAD
+    return {"file": "song.mp3", "contentType": "audio/mp3"}
+
+@app.route("/edit")
+def editAudioFile():
+    # TODO EDIT AUDIO FILE METADATA
+    return {"file": "song.mp3", "modified": True}
+
+@app.route("/search_meta")
+def searchAudioMetadata():
+    # TODO HANDLE AUDIO METADATA SEARCH - USE MUSICBRAINZ API FOR THIS 
+    return {"title": "You Can Always Come Home", "artist": "Toby Fox", "album": "DELTARUNE Chapter 1 (Original Game Soundtrack)"}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
