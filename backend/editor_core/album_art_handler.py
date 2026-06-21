7748fe3c971f6bf07586cd6ad4a304de01880196
emman-123 <medinaemmanruiz@gmail.com>
2026-06-21 22:47:05 +0100

    move audio tag name mappings and file extension mappings to a separate module

--- backend/editor_core/album_art_handler.py
+++ backend/editor_core/album_art_handler.py
@@ -1,3 +1,34 @@
+from mappings import AUDIO_TAG_MAPPING, EXT_TO_TYPE
+import os
+import mutagen
+import traceback
+
 class AlbumArtHandler:
     def __init__(self, audio_src):
         self.audio_src = audio_src
+
+    # GETTER methods for cover art
+    def _read_cover_art(self):
+        pass
+
+    def _read_id3_art(self):
+        pass
+
+    def _read_mp4_art(self):
+        pass
+
+    def _read_asf_art(self):
+        pass
+
+    # setter methods for cover art
+    def _set_cover_art(self):
+        pass
+
+    def _set_id3_art(self):
+        pass
+
+    def _set_mp4_art(self):
+        pass
+
+    def _set_asf_art(self):
+        pass
